a3728f100556a62a69cef6e57b73e42df55be4a4
aslihancelik <aslihancelik27@gmail.com>
2024-09-26 20:26:19 -0400

    revision completed

--- CSC221_Chapter2_Ex18.cpp
+++ CSC221_Chapter2_Ex18.cpp
@@ -1,21 +1,27 @@
-// CSC221_Chapter2_Ex18.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-// This program displays the approximate number of customers surveyed who:
+// CSC221_Chapter2_Ex18.cpp :  This program first asks for the number of 
+// customers surveyed to the user an uses the input to make calculations.
+// Then displays the approximate number of customers surveyed who:
 // 1) Purchase one or more energy drinks per week
 // 2) Preferring citrus-flavored energy drinks
 //
 // Course: CSC 221
 // Programmer: Aslihan Celik
 // Date: 09/26/2024
-// Assignment: Chapter 2 Exercise 18
+// Assignment: Chapter 2 Exercise 18 Revised
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    // Constant for total number of customers surveyed
-    const int TOTAL_CUSTOMER = 16500;
+    // Variable for total number of customers surveyed
+    int totalCustomer;
+
+    //Prompt the user to enter number of customers surveyed
+    cout << "Please enter the number of customers surveyed." << endl;
+    cin >> totalCustomer;
+    //Display the amount they entered.
+    cout << "You entered: " << totalCustomer << endl;
 
     // Constants for the percentage of surveyed people
     const double PERCENTAGE_ENERGY_DRINK = 0.15;  // Percentage purchasing one or more energy drinks per week
@@ -25,7 +31,7 @@ int main()
     int energyDrinkCustomer, citrusflavorCustomer;
 
     // Calculating the number of customers purchasing energy drinks
-    energyDrinkCustomer = TOTAL_CUSTOMER * PERCENTAGE_ENERGY_DRINK;
+    energyDrinkCustomer = totalCustomer * PERCENTAGE_ENERGY_DRINK;
 
     // Calculating the number of customers preferring citrus-flavored energy drinks
     citrusflavorCustomer = energyDrinkCustomer * PERCENTAGE_CITRUS_FLAVOR;
